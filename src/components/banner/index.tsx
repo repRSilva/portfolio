a2298394fa0151113d145f29b1c19c6f9d193d7a
Rafael <rafaelsilvamap@gmail.com>
2021-02-18 09:33:31 -0300

    Creating Banner component.

---
+++ src/components/banner/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Content } from './styles';
+
+const Banner: React.FC = () => (
+  <Container>
+    <Content>
+      <h1>Banner</h1>
+    </Content>
+  </Container>
+);
+
+export default Banner;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Banner from '../../components/banner';
 import Header from '../../components/header';
 import Container from './styles';
 
 const Home: React.FC = () => (
   <Container>
     <Header />
+    <Banner />
   </Container>
 );
 
