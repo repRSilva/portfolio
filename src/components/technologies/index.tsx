7a3956372a4c02869272d2d1c0bb093956fe6a8c
Rafael <rafaelsilvamap@gmail.com>
2021-02-19 18:29:45 -0300

    Styling technologies component.

--- src/components/technologies/index.tsx
+++ src/components/technologies/index.tsx
@@ -3,9 +3,8 @@ import { Container, Content } from './styles';
 
 const Technologies: React.FC = () => (
   <Container>
-    <Content>
-      <h1>Techs</h1>
-    </Content>
+    <h1>Tecnologias</h1>
+    <Content />
   </Container>
 );
 
--- src/components/technologies/styles.ts
+++ src/components/technologies/styles.ts
@@ -1,5 +1,28 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding: 100px 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #28262e;
 
-export const Content = styled.div``;
+  h1 {
+    font-size: 36px;
+    color: #06d6a0;
+  }
+`;
+
+export const Content = styled.div`
+  margin-top: 50px;
+  width: 100%;
+  padding: 30px;
+  border-radius: 10px;
+  box-shadow: 1px 1px 5px #999591;
+  transition: box-shadow 0.5s;
+
+  :hover {
+    box-shadow: 4px 4px 15px #06d6a0;
+  }
+`;
