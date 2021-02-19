f2528d93d32309c196ece11574a68b4e247c12c2
Rafael <rafaelsilvamap@gmail.com>
2021-02-19 13:55:11 -0300

    Creating footer component.

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Content } from './styles';
+
+const Footer: React.FC = () => (
+  <Container>
+    <Content>
+      <h1>Footer</h1>
+    </Content>
+  </Container>
+);
+
+export default Footer;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,6 +4,7 @@ import Banner from '../../components/banner';
 import Formation from '../../components/formation';
 import Certification from '../../components/certifications';
 import Container from './styles';
+import Footer from '../../components/footer';
 
 const Home: React.FC = () => (
   <Container>
@@ -11,6 +12,7 @@ const Home: React.FC = () => (
     <Banner />
     <Formation />
     <Certification />
+    <Footer />
   </Container>
 );
 
