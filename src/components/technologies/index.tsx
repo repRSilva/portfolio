d53ffa4f8b8aeb432d4202745963e167c1df1d80
Rafael <rafaelsilvamap@gmail.com>
2021-02-19 18:23:03 -0300

    Creating technologies component.

---
+++ src/components/technologies/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Content } from './styles';
+
+const Technologies: React.FC = () => (
+  <Container>
+    <Content>
+      <h1>Techs</h1>
+    </Content>
+  </Container>
+);
+
+export default Technologies;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import Container from './styles';
 import Header from '../../components/header';
 import Banner from '../../components/banner';
+import Technologies from '../../components/technologies';
 import Formation from '../../components/formation';
 import Certification from '../../components/certifications';
-import Container from './styles';
 import Footer from '../../components/footer';
 
 const Home: React.FC = () => (
   <Container>
     <Header />
     <Banner />
+    <Technologies />
     <Formation />
     <Certification />
     <Footer />
